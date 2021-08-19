d89cfac462377ac197ae58ac610d098894ddf99a
Marco <marco1996@gmail.com>
2021-08-19 17:59:18 -0400

    Second

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          HOLA PEDROnpm
+          LEARN REACT
         </a>
       </header>
     </div>
